3a64737fd666bc719c29ddb00e34b3ea41e0dfaa
kongfanzhi <463120197@qq.com>
2025-01-07 03:52:59 +0800

    first test

---
+++ source/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <glm/glm.hpp>
+
+int main()
+{
+    glm::vec3 test = {1,2,6};
+    std::cout<<"Hello world"<<std::endl;
+    std::cout<<test.x<<test.y<<test.z<<std::endl;
+    return 0;
+}
